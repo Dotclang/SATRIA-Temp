6d5add3e9814ea5d986f9651cf42a07099cdc1a2
Edy Ridwan Hutagalung <68416754+Dotclang@users.noreply.github.com>
2023-04-26 10:31:54 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    You're logged in!
+
+                    <a href="http://ems.test/authenticate?email={{auth()->user()->email}}&token={{base64_encode(Hash::make(config('auth.token')))}}"
+                        class='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-lime-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'>EMS Portal</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
